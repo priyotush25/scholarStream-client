de6a58d28e9770b90b4640d79b050a1d264756d3
Priyotush <priyotushpaul40@gmail.com>
2025-12-17 19:08:17 +0600

    add home page

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,42 +1,185 @@
-import React from "react";
 import { createBrowserRouter } from "react-router";
-import MainLayout from "../layouts/MainLayout";
-import Home from "../pages/Home/Home";
-import Login from "../pages/Login/Login";
-import Register from "../pages/Register/Register";
-import AllScholarships from "../pages/AllScholarships/AllScholarships";
-import AddScholarship from "../pages/AddScholarship/AddScholarship";
-import PrivateRoute from "./PrivateRoute";
+import Root from "../pages/Root/Root";
+import HomeLayout from "../Layouts/HomeLayout/HomeLayout";
+import Login from "../components/Auth/Login/Login";
+import Register from "../components/Auth/Register/Register";
+import AllScholarships from "../components/common/Scholarships/AllScholarships/AllScholarships";
+import ScholarshipDetails from "../components/common/Scholarships/ScholarshipDetails.jsx/ScholarshipDetails";
+import LearnMore from "../components/LandingPage/LearnMore";
+import PaymentSuccess from "../pages/Payments/PaymentSuccess";
+import PrivateRoutes from "../contexts/ProtecedRoutes/PrivateRoutes";
+import HowItWorks from "../pages/FooterPages/HowItWorks";
+import Blog from "../pages/FooterPages/Blog";
+import HelpCenter from "../pages/FooterPages/HelpCenter";
+import ContactUs from "../pages/FooterPages/ContactUs";
+import SuccessStories from "../pages/FooterPages/SuccessStories";
+import PrivacyPolicy from "../pages/FooterPages/PrivacyPolicy";
+import Loader from "../components/common/Loader/Loader";
+import Error from "../pages/Errors/Error";
+import Forbidden from "../pages/Errors/Forbidden";
+import Unauthorized from "../pages/Errors/Unauthorized";
+import MainDashboard from "../Layouts/Dashboard/MainDashboard";
+import MyProfile from "../pages/Dashboard/common/MyProfile";
+import MyApplications from "../components/Student/MyApplications";
+import DashboardHome from "../pages/Dashboard/DashboardHome";
+import PaymentFailed from "../pages/Payments/PaymentFailed";
+import MyReviews from "../components/Student/MyReviews";
+import AddScholarship from "../pages/Dashboard/Admin/AddScholarship";
+import ManageScholarships from "../pages/Dashboard/Admin/ManageScholarships";
+import ManageUsers from "../pages/Dashboard/Admin/ManageUsers";
+import ManageApplications from "../pages/Dashboard/Moderator/ManageApplications";
+import AllReviews from "../pages/Dashboard/Moderator/AllReviews";
+import AdminRoute from "../contexts/ProtecedRoutes/AdminRoute";
+import ModeratorRoute from "../contexts/ProtecedRoutes/ModeratorRoute";
 
-const Router = createBrowserRouter([
+const router = createBrowserRouter([
   {
     path: "/",
-    element: <MainLayout />,
+    hydrateFallbackElement: <Loader />,
+    errorElement: <Error />,
+    Component: Root,
     children: [
       {
-        path: "/",
-        element: <Home />,
+        index: true,
+        Component: HomeLayout,
       },
       {
         path: "/login",
-        element: <Login />,
+        Component: Login,
       },
       {
         path: "/register",
-        element: <Register />,
+        Component: Register,
       },
       {
-        path: "/allscholarship",
-        element: <AllScholarships/>
+        path: "/how-it-works",
+        Component: HowItWorks,
       },
       {
-        path: "/addcholarship",
-        element: <PrivateRoute>
-          <AddScholarship/>
-        </PrivateRoute>
-      }
+        path: "/faq",
+        Component: LearnMore,
+      },
+      {
+        path: "/blog",
+        Component: Blog,
+      },
+      {
+        path: "/help-center",
+        Component: HelpCenter,
+      },
+      {
+        path: "/contact-us",
+        Component: ContactUs,
+      },
+      {
+        path: "/success-stories",
+        Component: SuccessStories,
+      },
+      {
+        path: "/privacy-policy",
+        Component: PrivacyPolicy,
+      },
+      {
+        path: "/forbidden",
+        Component: Forbidden,
+      },
+      {
+        path: "/unauthorized",
+        Component: Unauthorized,
+      },
+
+      {
+        path: "/all-scholarships",
+        element: (
+          <PrivateRoutes>
+            <AllScholarships />
+          </PrivateRoutes>
+        ),
+      },
+      {
+        path: "/scholarship-details/:id",
+        element: (
+          <PrivateRoutes>
+            <ScholarshipDetails />
+          </PrivateRoutes>
+        ),
+      },
+      {
+        path: "/payment-success",
+        Component: PaymentSuccess,
+      },
+      {
+        path: "/payment-cancelled",
+        Component: PaymentFailed,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <PrivateRoutes>
+        <MainDashboard />
+      </PrivateRoutes>
+    ),
+    children: [
+      {
+        index: true,
+        Component: DashboardHome,
+      },
+      {
+        path: "my-profile",
+        Component: MyProfile,
+      },
+      {
+        path: "my-applications",
+        Component: MyApplications,
+      },
+      {
+        path: "my-reviews",
+        Component: MyReviews,
+      },
+      {
+        path: "add-scholarship",
+        element: (
+          <AdminRoute>
+            <AddScholarship />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manage-scholarships",
+        element: (
+          <AdminRoute>
+            <ManageScholarships />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manage-users",
+        element: (
+          <AdminRoute>
+            <ManageUsers />
+          </AdminRoute>
+        ),
+      },
+      {
+        path: "manage-applications",
+        element: (
+          <ModeratorRoute>
+            <ManageApplications />
+          </ModeratorRoute>
+        ),
+      },
+      {
+        path: "all-reviews",
+        element: (
+          <ModeratorRoute>
+            <AllReviews />
+          </ModeratorRoute>
+        ),
+      },
     ],
   },
 ]);
 
-export default Router;
+export default router;
